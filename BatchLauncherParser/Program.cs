baddb066ebdef4e7aea9a3a8a2d4132f5ac090e3
Zackin5 <zachin55@gmail.com>
2021-06-02 19:17:08 -0500

    Parser updates

--- BatchLauncherParser/Program.cs
+++ BatchLauncherParser/Program.cs
@@ -20,31 +20,37 @@ namespace BatchLauncherParser
 
             var fileLines = File.ReadAllLines(filePath);
 
-            var mods = new List<Mod>();
+            var parsedConfig = new LauncherConfig
+            {
+                Mods = new List<Mod>(),
+                Levels = new List<Mod>(),
+                Mutators = new List<Mod>()
+            };
 
             // Parse mods
-            mods.AddRange(ParseModRange(511, 866, fileLines));
+            parsedConfig.Mods.AddRange(ParseModRange(511, 866, fileLines));
 
             // Add mod attributes
-            var titles = ParseModTitleRange(19, 24, fileLines, "Mostly Vanilla");
-            titles.AddRange(ParseModTitleRange(27, 97, fileLines, "Gameplay Mods"));
-            titles.AddRange(ParseModTitleRange(100, 106, fileLines, "Total Conversions"));
-            titles.AddRange(ParseModTitleRange(109, 113, fileLines, "BRUTAL DOOM"));
+            var modTitles = ParseModTitleRange(19, 24, fileLines, "Mostly Vanilla");
+            modTitles.AddRange(ParseModTitleRange(27, 97, fileLines, "Gameplay Mods"));
+            modTitles.AddRange(ParseModTitleRange(100, 106, fileLines, "Total Conversions"));
+            modTitles.AddRange(ParseModTitleRange(109, 113, fileLines, "BRUTAL DOOM"));
 
-            for (var index = 0; index < mods.Count; index++)
-            {
-                var title = titles.FirstOrDefault(f => f.code.Equals(mods[index].Code, StringComparison.OrdinalIgnoreCase));
+            parsedConfig.Mods = MapModTitles(parsedConfig.Mods, modTitles);
 
-                mods[index].Description = title.title;
-                mods[index].Category = title.category;
+            // Parse levels
+            parsedConfig.Levels.AddRange(ParseLevelRange(331, 445, fileLines));
 
-                var modPath = mods[index].Path.FirstOrDefault();
+            var levelTitles = ParseModTitleRange(173, 183, fileLines, "1990's");
+            levelTitles.AddRange(ParseModTitleRange(194, 224, fileLines, "2000's"));
+            levelTitles.AddRange(ParseModTitleRange(235, 300, fileLines, "2010's"));
+            levelTitles.AddRange(ParseModTitleRange(303, 303, fileLines, "Cacowards 25"));
+            levelTitles.AddRange(ParseModTitleRange(306, 311, fileLines, "Cacowards 25"));
 
-                if (!string.IsNullOrWhiteSpace(modPath))
-                    mods[index].Year = File.GetCreationTime(modPath).Year;
-            }
+            parsedConfig.Levels = MapModTitles(parsedConfig.Levels, levelTitles);
 
-            var output = JsonConvert.SerializeObject(mods, Formatting.Indented, new JsonSerializerSettings
+            // Dump JSON
+            var output = JsonConvert.SerializeObject(parsedConfig, Formatting.Indented, new JsonSerializerSettings
             {
                 NullValueHandling = NullValueHandling.Ignore
             });
@@ -56,7 +62,7 @@ namespace BatchLauncherParser
         {
             var names = new List<(string code, string title, string category)>();
 
-            for (var i = start; i < end; i++)
+            for (var i = start - 1; i < end; i++)
             {
                 names.Add((fileLines[i].Substring(5, 4).Trim(), fileLines[i].Substring(12), category));
             }
@@ -83,9 +89,7 @@ namespace BatchLauncherParser
                     continue;
                 else if (activeLine.StartsWith("start", StringComparison.OrdinalIgnoreCase))
                 {
-                    var lineParts = Regex.Matches(activeLine, @"[\""].+?[\""]|[^ ]+")
-                        .Select(m => m.Value)
-                        .ToList();
+                    var lineParts = SplitString(activeLine);
 
                     foreach (var linePart in lineParts)
                     {
@@ -108,5 +112,59 @@ namespace BatchLauncherParser
 
             return mods;
         }
+
+        private static List<Mod> ParseLevelRange(int start, int end, IReadOnlyList<string> fileLines)
+        {
+            var mods = new List<Mod>();
+
+            for (var i = start; i < end; i++)
+            {
+                var wipMod = new Mod
+                {
+                    Path = new List<string>(),
+                    Tags = new List<string>()
+                };
+
+                var mapItems = SplitString(fileLines[i]);
+
+                var codeIndex = mapItems.FindIndex(f => f.Contains("%CHOICE%", StringComparison.OrdinalIgnoreCase));
+                var iwadIndex = mapItems.FindIndex(f => f.Contains("-iwad", StringComparison.OrdinalIgnoreCase));
+
+                wipMod.Code = mapItems[codeIndex].Substring(10).Trim();
+
+                if (iwadIndex != -1)
+                    wipMod.IWad = mapItems.ElementAtOrDefault(iwadIndex + 1);
+
+                wipMod.Path = mapItems.Where(f => f.StartsWith(@"E:\DOOM", StringComparison.OrdinalIgnoreCase)).ToList();
+
+                mods.Add(wipMod);
+            }
+
+            return mods;
+
+        }
+
+        private static List<Mod> MapModTitles(List<Mod> modList, List<(string code, string title, string category)> titles)
+        {
+            for (var index = 0; index < modList.Count; index++)
+            {
+                var title = titles.FirstOrDefault(f => f.code.Equals(modList[index].Code, StringComparison.OrdinalIgnoreCase));
+
+                modList[index].Description = title.title;
+                modList[index].Category = title.category;
+
+                var modPath = modList[index].Path.FirstOrDefault();
+
+                if (!string.IsNullOrWhiteSpace(modPath))
+                    modList[index].Year = File.GetCreationTime(modPath).Year;
+            }
+
+            return modList;
+        }
+
+        private static List<string> SplitString(string str) => 
+            Regex.Matches(str, @"[\""].+?[\""]|[^ ]+")
+            .Select(m => m.Value)
+            .ToList();
     }
 }
